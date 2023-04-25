4b94ed78b7c79d5fbb17f550a30ebb812ca4aaa7
NyiWaiYan <nyiwaiyan2212@gmail.com>
2023-04-25 12:11:31 +0630

    login and signup style

---
+++ src/components/Login.vue
@@ -0,0 +1,30 @@
+<template>
+    <h2>Login Form</h2>
+    <form action="" @submit.prevent="Login">
+     
+        <input type="email" placeholder="Enter Email" v-model="email">
+        <input type="password" placeholder="Enter Password" v-model="password">
+        <button> Log In</button>
+    </form>
+</template>
+
+<script>
+import { ref } from 'vue'
+export default {
+    setup(){
+        let email= ref("");
+        let password= ref("");
+        let Login=()=>{
+            console.log(email.value,password.value)
+        }
+        
+        return {email,password,Login}
+    }
+
+   
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Signup.vue
@@ -0,0 +1,31 @@
+<template>
+    <h2>Sign Up Form</h2>
+    <form action="" @submit.prevent="signUp">
+        <input type="text" placeholder="Enter Display Name" v-model="displayname">
+        <input type="email" placeholder="Enter Email" v-model="email">
+        <input type="password" placeholder="Enter Password" v-model="password">
+        <button> Sign Up</button>
+    </form>
+</template>
+
+<script>
+import { ref } from 'vue'
+export default {
+    setup(){
+        let displayname = ref("");
+        let email= ref("");
+        let password= ref("");
+        let signUp=()=>{
+            console.log(displayname.value,email.value,password.value)
+        }
+        
+        return {displayname,email,password,signUp}
+    }
+
+   
+}
+</script>
+
+<style>
+
+</style>
